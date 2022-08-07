e43372bb039b0a80e1c81a395e804c7b38282855
Kal Tadesse <kalkidantadesse@Kalkidans-MacBook-Pro.local>
2022-08-07 12:55:17 -0400

    Integration of lift plot with the project

--- predictAttrition.R
+++ predictAttrition.R
@@ -1,5 +1,11 @@
 "This script will deal with only predicting the attrition when new dataset is given to the model upon deployment"
 
+library(caTools)
+library(ROCR)
+library(dplyr)
+library(ggplot2)
+library(MASS)
+
 #@author Kal Tadesse
 
 #Date Created: 07/29/2022
@@ -23,8 +29,28 @@ predictWithNewData <- predict(model, cleanedTestData, type = "response")
 predictionProbability <- ifelse(predictWithNewData > 0.5, 1, 0)
 
 #OverTime - Confusion Matrix - gives us true positives and negatives as well as false positives and negatives
-table(cleanedTestData$OverTime, predictionProbability)
+table(cleanedTestData$Attrition, predictionProbability)
 
 #Find accuracy for the attrition predictive based on attrition
-missingClassError <- mean(predictionProbability != cleanedTestData$OverTime)
+missingClassError <- mean(predictionProbability != cleanedTestData$Attrition)
 print(paste('Accuracy =', 1 - missingClassError))
+
+#ROC and AUC curve
+rocPrediction <- prediction(predictionProbability, cleanedTestData$Attrition)
+rocPredictedPerformance <- performance(rocPrediction, measure = "tpr", x.measure = "fpr")
+
+aucPerfomrance <- performance(rocPrediction, measure = "auc")
+aucPerfomrance <- aucPerfomrance@y.values[[1]]
+
+#aucPerfomrance
+
+pred <- performance(rocPrediction, "lift", "rpp")
+
+#uncomment this line to draw lift 
+#Plotting different curves
+#plot(pred, colorize = TRUE, main = "Lift Curve - Attrition")
+plot(rocPredictedPerformance, colorize = TRUE, print.cutoffs.at = seq(0.1, by = 0.1), main = "ROC CURVE - Attrition")
+abline(a = 0, b = 1)
+
+aucPerfomrance <- round(aucPerfomrance, 4)
+legend(0.6, 0.4, aucPerfomrance, title = "AUC - Attrition", cex = 1)
